Update metrics calculation for missing data. Change loss function. Fix the ClerML excessive uploading.

src/data/traffic_datamodule.py
@@ -27,8 +27,7 @@ class TrafficDataModule(pl.LightningDataModule):
 
         self.opt = opt
         self.dataset = dataset
-        self.train_batch_size = opt["train_batch_size"]
-        self.test_batch_size = opt["test_batch_size"]
+        self.batch_size = opt["batch_size"]
 
         self.train_dataset: Optional[Dataset] = None
         self.valid_dataset: Optional[Dataset] = None
@@ -96,8 +95,14 @@ class TrafficDataModule(pl.LightningDataModule):
         self.train_dataset = Subset(self.dataset, train_indices)
 
         train_features = torch.stack([x[:, :, 0] for x, _ in self.train_dataset])
-        self.train_mean = torch.mean(train_features)
-        self.train_std = torch.std(train_features)
+        nonzero = torch.count_nonzero(train_features)
+
+        nonzero_indices = train_features.nonzero(as_tuple=True)
+        nonzero_features = train_features[nonzero_indices].view(-1)
+
+        self.train_mean = torch.sum(train_features) / nonzero
+        self.train_std = torch.sqrt(torch.sum(nonzero_features - self.train_mean) ** 2 /
+                                    nonzero)
 
         self.valid_dataset = Subset(self.dataset, valid_indices)
         self.test_dataset = Subset(self.dataset, test_indices)
@@ -139,7 +144,8 @@ class TrafficDataModule(pl.LightningDataModule):
         x_signal = torch.stack([self.normalize(x_[..., 0]) for x_ in x]).unsqueeze(
             dim=-1
         )
-        y_signal = torch.stack([y_[..., 0] for y_ in y]).unsqueeze(dim=-1)
+        y_signal = torch.stack([self.normalize(y_[..., 0]) for y_ in y]).unsqueeze(
+            dim=-1)
 
         x_temporal = torch.stack([self.onehot_temporal(x_[..., 1:]) for x_ in x])
         y_temporal = torch.stack([self.onehot_temporal(y_[..., 1:]) for y_ in y])
@@ -155,7 +161,7 @@ class TrafficDataModule(pl.LightningDataModule):
         :return: A tuple containing the x and y tensors.
         """
         x_signal = torch.stack([self.normalize(data.x[..., 0]) for data in batch])
-        y_signal = torch.stack([data.x[..., 0] for data in batch])
+        y_signal = torch.stack([self.normalize(data.x[..., 0]) for data in batch])
 
         x_temporal = torch.stack(
             [self.onehot_temporal(data.x[..., 1:]) for data in batch]
@@ -174,7 +180,7 @@ class TrafficDataModule(pl.LightningDataModule):
         """
         return DataLoader(
             self.train_dataset,
-            batch_size=self.train_batch_size,
+            batch_size=self.batch_size,
             num_workers=2,
             shuffle=True,
             pin_memory=True,
@@ -189,7 +195,7 @@ class TrafficDataModule(pl.LightningDataModule):
         """
         return DataLoader(
             self.valid_dataset,
-            batch_size=self.test_batch_size,
+            batch_size=self.batch_size,
             num_workers=2,
             collate_fn=self.collate_fn,
         )
@@ -202,7 +208,7 @@ class TrafficDataModule(pl.LightningDataModule):
         """
         return DataLoader(
             self.test_dataset,
-            batch_size=self.test_batch_size,
+            batch_size=self.batch_size,
             num_workers=2,
             collate_fn=self.collate_fn,
         )

src/experiment.py
@@ -16,6 +16,8 @@ from torch_geometric.datasets import MetrLa, MetrLaInMemory, PemsBay, PemsBayInM
 
 from src.model.gman.gman_linear import FastLinearGMAN
 from src.model.gman.gatman import GATMAN
+from src.util.masked_metrics import masked_rmse, masked_mae, masked_mape, \
+    masked_mae_loss
 from src.util.utils import get_number_of_nodes
 from src.data.traffic_datamodule import TrafficDataModule
 from src.model import (
@@ -134,6 +136,26 @@ class Experiment:
             mean_metric_value[key] = np.mean([item[key] for item in values])
         return mean_metric_value
 
+    @staticmethod
+    def compute_metrics(y, y_hat):
+        rmses = {
+            k: masked_rmse(y[:, v, :], y_hat.detach()[:, v, :])
+            for k, v in indices.items()
+        }
+
+        maes = {
+            k: masked_mae(y[:, v, :], y_hat.detach()[:, v, :])
+            for k, v in indices.items()
+        }
+
+        mapes = {
+            k: masked_mape(y[:, v, :], y_hat.detach()[:, v, :])
+            for k, v in indices.items()
+        }
+
+        metrics = {"rmses": rmses, "maes": maes, "mapes": mapes}
+        return metrics
+
     def common_step(
         self, batch: List[torch.Tensor], pos_encoding=None, use_best_model: bool = False
     ) -> Tuple[torch.tensor, dict]:
@@ -162,30 +184,10 @@ class Experiment:
                 if not use_best_model
                 else self.best_model(x_signal)
             )
-        loss = mse_loss(y_signal, y_hat)
+        loss = masked_mae_loss(y_signal, y_hat)
 
-        rmses = {
-            k: torch.sqrt(
-                torch.mean((y_signal[:, v, :] - y_hat.detach()[:, v, :]) ** 2)
-            ).item()
-            for k, v in indices.items()
-        }
-
-        maes = {
-            k: torch.mean(torch.abs(y_signal[:, v, :] - y_hat.detach()[:, v, :])).item()
-            for k, v in indices.items()
-        }
-
-        mapes = {
-            k: torch.mean(
-                torch.abs(y_signal[:, v, :] - y_hat.detach()[:, v, :])
-                / (y_signal[:, v, :] + 1e-3)
-            ).item()
-            for k, v in indices.items()
-        }
-
+        metrics = self.compute_metrics(y_signal, y_hat)
         del x_signal, y_signal, y_hat, x_temporal, y_temporal
-        metrics = {"rmses": rmses, "maes": maes, "mapes": mapes}
         return loss, metrics
 
     def train_step(
@@ -305,13 +307,14 @@ class Experiment:
         Initializes the dataset, datamodule, experiment versioning,
         :return:
         """
-        tag = binascii.b2a_hex(os.urandom(3))
+        tag = binascii.b2a_hex(os.urandom(3)).decode("utf-8")
         self.task = Task.init(
             project_name="Graph Diffusion Traffic Forecasting",
             task_name=f"Train Task {tag}",
             task_type=TaskTypes.training,
             reuse_last_task_id=False,
             output_uri="s3://traffic-models",
+            auto_connect_frameworks={"pytorch" : ['best_model.pt']}
         )
         self.clearml_logger = self.task.logger
 
@@ -480,6 +483,7 @@ class Experiment:
             f"Best epoch: {self.best_model_info['Epoch']}. Mean RMSE: {self.best_val_rmse}"
         )
         if not early_stopping.early_stop:
+            print("Saving model...")
             torch.save(self.best_model.state_dict(), self.model_path)
             # self.task.update_output_model(self.model_path, "Model")
         self.task.update_output_model(self.model_path)